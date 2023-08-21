f4ba263697a7c20965c9017825869064c7e8aa7f
Daniel German <danielchuksgerman@gmail.com>
2023-08-21 05:29:34 -0700

    Fix get_print_width.c

--- get_print_width.c
+++ get_print_width.c
@@ -11,28 +11,28 @@
 
 int get_print_width(const char *format, int *formatIndex, va_list list)
 {
-        int current_index;
-        int width = 0;
+	int current_index;
+	int width = 0;
 
-        for (current_index = *formatIndex + 1;
-                        format[current_index] != '\0'; current_index++)
-        {
-                if (is_digit(format[current_index]))
-                {
-                        width *= 10;
-                        width += format[current_index] - '0';
-                }
-                else if (format[current_index] == '*')
-                {
-                        current_index++;
-                        width = va_arg(list, int);
-                        break;
-                }
-                else
-                        break;
-        }
+	for (current_index = *formatIndex + 1;
+		 format[current_index] != '\0'; current_index++)
+	{
+		if (is_digit(format[current_index]))
+		{
+			width *= 10;
+			width += format[current_index] - '0';
+		}
+		else if (format[current_index] == '*')
+		{
+			current_index++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-        *formatIndex = current_index - 1;
+	*formatIndex = current_index - 1;
 
-        return (width);
+	return (width);
 }
