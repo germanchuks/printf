bf5cd22e84fb6d617ece8ecb4e8c9bb01a6fc5f5
stuckwithprogression <chukwunwemichael@gmail.com>
2023-08-21 05:18:42 -0700

    Fix print_func_2.c

--- printf_func_2.c
+++ printf_func_2.c
@@ -1,199 +1,165 @@
 #include "main.h"
 
-
 /**
- * print_reversed_string - Prints string in reverse order.
- * @args: The list of arguments
- * @output_buffer: The buffer array to handle print (UNUSED)
- * @format_flag:  The active flags
- * @min_width: The width specification (UNUSED)
- * @precision: The precision specification (UNUSED)
- * @size_spec: The size specifier (UNUSED)
- * Return: Numbers of characters printed
- */
-
-int print_reversed_string(va_list args, char output_buffer[],
-                                  int format_flag, int min_width, int precision, int size_spec)
-{
-        char *str;
-        int i, count = 0;
-
-        UNUSED(output_buffer);
-        UNUSED(format_flag);
-        UNUSED(min_width);
-        UNUSED(size_spec);
-
-        str = va_arg(args, char *);
-
-        if (str == NULL)
-        {
-                UNUSED(precision);
-                str = ")Null(";
-        }
-
-        /* Determines length of the string */
-        for (i = 0; str[i]; i++)
-        {
-        }
-
-        for (i = i - 1; i >= 0; i--)
-        {
-                char character = str[i];
-
-                write(1, &character, 1);
-                count++;
-        }
-        return (count);
-}
-
-
-/**
- * print_rot13string - Print a string in ROT13 encoding.
- * @args: The list of arguments
- * @output_buffer: The buffer array (UNUSED)
- * @format_flag:  The active flags (UNUSED)
- * @min_width: The width spec (UNUSED)
- * @precision: The precision specification (UNUSED)
- * @size_spec: The size specifier (UNUSED)
+ * print_unsigned_int - Prints an unsigned number
+ * @args: The List of arguments
+ * @output_buffer: The buffer array to handle print
+ * @format_flags:  The active flags
+ * @min_width: The width specification
+ * @precision: The precision specification
+ * @size_spec: The size specifier
  *
  * Return: Number of characters printed.
  */
 
-int print_rot13string(va_list args, char output_buffer[],
-                                          int format_flag, int min_width, int precision, int size_spec)
+int print_unsigned_int(va_list args, char output_buffer[],
+					   int format_flags, int min_width, int precision, int size_spec)
 {
-        char x;
-        char *str;
-        unsigned int i, j;
-        int count = 0;
-        char original[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(args, unsigned long int);
 
-        str = va_arg(args, char *);
-        UNUSED(output_buffer);
-        UNUSED(format_flag);
-        UNUSED(precision);
-        UNUSED(size_spec);
-        UNUSED(min_width);
+	num = size_conv_unsgned(num, size_spec);
+	if (num == 0)
+		output_buffer[i--] = '0';
 
-        if (str == NULL)
-                str = "(AHYY)";
-        for (i = 0; str[i]; i++)
-        {
-                for (j = 0; original[j]; j++)
-                {
-                        if (original[j] == str[i])
-                        {
-                                x = rot13[j];
-                                write(1, &x, 1);
-                                count++;
-                                break;
-                        }
-                }
-
-                /* If character is not in original, print as it is*/
-                if (!original[j])
-                {
-                        x = str[i];
-                        write(1, &x, 1);
-                        count++;
-                }
-        }
-        return (count);
+	output_buffer[BUFF_SIZE - 1] = '\0';
+	while (num > 0)
+	{
+		output_buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+	i++;
+	return (write_unsigned_int(0, i, output_buffer, format_flags,
+							   min_width, precision, size_spec));
 }
 
-
-
 /**
- * print_pointer - Prints the value of a pointer variable
+ * print_octal_number - Prints unsigned number in octal format.
  * @args: The list of arguments
  * @output_buffer: The buffer array to handle print
- * @format_flag:  The active flags
- * @min_width: The width specification (UNUSED)
- * @precision: The precision specification (UNUSED)
- * @size_spec: The size specifier (UNUSED)
+ * @format_flags:  The active flags
+ * @min_width: The width Specification
+ * @precision: The precision specification
+ * @size_spec: The size specifier
  *
  * Return: Number of characters printed.
  */
 
-int print_pointer(va_list args, char output_buffer[],
-                                  int format_flag, int min_width, int precision, int size_spec)
+int print_octal_number(va_list args, char output_buffer[],
+					   int format_flags, int min_width, int precision, int size_spec)
 {
-        char extra_c = 0, padding_char = ' ';
-        int index = BUFF_SIZE - 2, length = 2, padding_start = 1;
-        unsigned long num_address;
-        char hex_map[] = "0123456789abcdef";
-        void *address = va_arg(args, void *);
 
-        UNUSED(size_spec);
-        UNUSED(min_width);
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(args, unsigned long int);
+	unsigned long int originalNum = num;
 
-        if (address == NULL)
-                return (write(1, "(nil)", 5));
+	UNUSED(min_width);
 
-        output_buffer[BUFF_SIZE - 1] = '\0';
-        UNUSED(precision);
+	num = size_conv_unsgned(num, size_spec);
 
-        num_address = (unsigned long)address;
+	if (num == 0)
+		output_buffer[i--] = '0';
 
-        while (num_address > 0)
-        {
-                output_buffer[index--] = hex_map[num_address % 16];
-                num_address /= 16;
-                length++;
-        }
+	output_buffer[BUFF_SIZE - 1] = '\0';
 
-        /* Handle formatting flags */
-        if ((format_flag & FLAG_ZERO) && !(format_flag & FLAG_MINUS))
-                padding_char = '0';
-        if (format_flag & FLAG_PLUS)
-                extra_c = '+', length++;
-        else if (format_flag & FLAG_SPACE)
-                extra_c = ' ', length++;
+	while (num > 0)
+	{
+		output_buffer[i--] = (num % 8) + '0';
+		num /= 8;
+	}
 
-        index++;
+	if (format_flags & FLAG_HASH && originalNum != 0)
+		output_buffer[i--] = '0';
 
-        return (write_formatted_ptr(output_buffer, index, length,
-                                                  min_width, format_flag, padding_char, extra_c, padding_start));
+	i++;
+
+	return (write_unsigned_int(0, i, output_buffer, format_flags,
+							   min_width, precision, size_spec));
 }
 
 /**
- * print_non_printable - Prints ASCII codes in hex of non-printable characters
+ * print_hexadecimal - Prints an unsigned number in hexadecimal notation
  * @args: The list of arguments
  * @output_buffer: The buffer array to handle print
- * @format_flag:  The active flags (UNUSED)
- * @min_width: The width specification (UNUSED)
- * @precision: The precision specification (UNUSED)
- * @size_spec: The size specifier (UNUSED)
+ * @format_flags:  The active flags
+ * @min_width: The width specification
+ * @precision: The precision specification
+ * @size_spec: The size specifier
+ *
+ * Return: Number of characters printed.
+ */
+
+int print_hexadecimal(va_list args, char output_buffer[],
+					  int format_flags, int min_width, int precision, int size_spec)
+{
+	return (print_custom_hex(args, "0123456789abcdef", output_buffer,
+							 format_flags, 'x', min_width, precision, size_spec));
+}
+
+/**
+ * print_uppercase_hex - Prints an unsigned num in upper hexadecimal notation
+ * @args: The list of arguments
+ * @output_buffer: The buffer array to handle print
+ * @format_flags: The active flags
+ * @min_width: The width specification
+ * @precision: The precision specification
+ * @size_spec: The size specifier
  *
  * Return: Number of characters printed
  */
 
-int print_non_printable(va_list args, char output_buffer[],
-                                                int format_flag, int min_width, int precision, int size_spec)
+int print_uppercase_hex(va_list args, char output_buffer[],
+						int format_flags, int min_width, int precision, int size_spec)
 {
-        int i = 0, hex_offset = 0;
-        char *str = va_arg(args, char *);
+	return (print_custom_hex(args, "0123456789ABCDEF", output_buffer,
+							 format_flags, 'X', min_width, precision, size_spec));
+}
 
-        UNUSED(format_flag);
-        UNUSED(min_width);
-        UNUSED(precision);
-        UNUSED(size_spec);
+/**
+ * print_custom_hex - Prints a hexadecimal number in lower or upper case.
+ * @args: The list of arguments
+ * @map_to: The array of values to map the number to
+ * @output_buffer: The buffer array to handle print
+ * @format_flags:  The active flags
+ * @flag_ch: The flag character for prefix
+ * @min_width: The width specification
+ * @precision: The precision specification
+ * @size_spec: The size specifier
+ *
+ * Return: Number of characters printed.
+ */
 
-        if (str == NULL)
-                return (write(1, "(null)", 6));
+int print_custom_hex(va_list args, char map_to[], char output_buffer[],
+					 int format_flags, char flag_ch, int min_width,
+					 int precision, int size_spec)
+{
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(args, unsigned long int);
+	unsigned long int originalNum = num;
 
-        while (str[i] != '\0')
-        {
-                if (is_printable(str[i]))
-                        output_buffer[i + hex_offset] = str[i];
-                else
-                        hex_offset += append_hex_code(str[i], output_buffer, i + hex_offset);
+	UNUSED(min_width);
 
-                i++;
-        }
+	num = size_conv_unsgned(num, size_spec);
 
-        output_buffer[i + hex_offset] = '\0';
+	if (num == 0)
+		output_buffer[i--] = '0';
 
-        return (write(1, output_buffer, i + hex_offset));
+	output_buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		output_buffer[i--] = map_to[num % 16];
+		num /= 16;
+	}
+
+	if (format_flags & FLAG_HASH && originalNum != 0)
+	{
+		output_buffer[i--] = flag_ch;
+		output_buffer[i--] = '0';
+	}
+
+	i++;
+
+	return (write_unsigned_int(0, i, output_buffer, format_flags,
+							   min_width, precision, size_spec));
 }
