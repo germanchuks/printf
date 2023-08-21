c6db8030307e6c3fc815c2f2166e86320ec7d01a
stuckwithprogression <chukwunwemichael@gmail.com>
2023-08-21 05:14:14 -0700

    Fix utilities.c

--- utilities.c
+++ utilities.c
@@ -9,10 +9,10 @@
 
 int is_digit(char c)
 {
-        if (c >= '0' && c <= '9')
-                return (1);
+	if (c >= '0' && c <= '9')
+		return (1);
 
-        return (0);
+	return (0);
 }
 
 /**
@@ -24,10 +24,10 @@ int is_digit(char c)
 
 int is_printable(char c)
 {
-        if (c >= 32 && c < 127)
-                return (1);
+	if (c >= 32 && c < 127)
+		return (1);
 
-        return (0);
+	return (0);
 }
 
 /**
@@ -40,16 +40,16 @@ int is_printable(char c)
 
 long int size_conv_number(long int num, int target_size)
 {
-        if (target_size == SIZE_LONG)
-                return (num);
-        else if (target_size == SIZE_SHORT)
-                return ((short)num);
+	if (target_size == SIZE_LONG)
+		return (num);
+	else if (target_size == SIZE_SHORT)
+		return ((short)num);
 
-        return ((int)num);
+	return ((int)num);
 }
 
 /**
- * size_conv_unsigned - Converts an unsigned number to the specified size.
+ * size_conv_unsgned - Converts an unsigned number to the specified size.
  * @num: Number to be converted.
  * @size: Target size for conversion
  *
@@ -58,11 +58,11 @@ long int size_conv_number(long int num, int target_size)
 
 long int size_conv_unsgned(unsigned long int num, int size)
 {
-        if (size == SIZE_LONG)
-                return (num);
-        else if (size == SIZE_SHORT)
-                return ((unsigned short)num);
-        return ((unsigned int)num);
+	if (size == SIZE_LONG)
+		return (num);
+	else if (size == SIZE_SHORT)
+		return ((unsigned short)num);
+	return ((unsigned int)num);
 }
 
 /**
@@ -75,16 +75,16 @@ long int size_conv_unsgned(unsigned long int num, int size)
 
 int append_hex_code(char ascii_code, char output_buffer[], int index)
 {
-        char map_to[] = "0123456789ABCDEF";
+	char map_to[] = "0123456789ABCDEF";
 
-        if (ascii_code < 0)
-                ascii_code *= -1;
+	if (ascii_code < 0)
+		ascii_code *= -1;
 
-        output_buffer[index++] = '\\';
-        output_buffer[index++] = 'x';
+	output_buffer[index++] = '\\';
+	output_buffer[index++] = 'x';
 
-        output_buffer[index++] = map_to[ascii_code / 16];
-        output_buffer[index] = map_to[ascii_code % 16];
+	output_buffer[index++] = map_to[ascii_code / 16];
+	output_buffer[index] = map_to[ascii_code % 16];
 
-        return (1);
+	return (1);
 }
