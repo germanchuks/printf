e9f64bed9cbd27840e1a12986aa7b3ce869a3ed4
Daniel German <danielchuksgerman@gmail.com>
2023-08-21 05:28:43 -0700

    Fix get_print_precision.c

--- get_print_precision.c
+++ get_print_precision.c
@@ -11,33 +11,32 @@
 
 int get_print_precision(const char *format, int *index, va_list list)
 {
-        int currentIndex = *index + 1;
-        int precision = -1;
+	int currentIndex = *index + 1;
+	int precision = -1;
 
-        if (format[currentIndex] != '.')
-                return (precision);
+	if (format[currentIndex] != '.')
+		return (precision);
 
-        precision = 0;
+	precision = 0;
 
-        for (currentIndex += 1; format[currentIndex] != '\0'; currentIndex++)
-        {
-                if (is_digit(format[currentIndex]))
-                {
-                        precision *= 10;
-                        precision += format[currentIndex] - '0';
-                }
-                else if (format[currentIndex] == '*')
-                {
-                        currentIndex++;
-                        precision = va_arg(list, int);
-                        break;
-                }
-                else
-                        break;
-        }
+	for (currentIndex += 1; format[currentIndex] != '\0'; currentIndex++)
+	{
+		if (is_digit(format[currentIndex]))
+		{
+			precision *= 10;
+			precision += format[currentIndex] - '0';
+		}
+		else if (format[currentIndex] == '*')
+		{
+			currentIndex++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-        *index = currentIndex - 1;
+	*index = currentIndex - 1;
 
-        return (precision);
+	return (precision);
 }
-
