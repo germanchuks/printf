b03a8715f4a08f990c4f9bf0b7922c14dd849fc5
Daniel German <danielchuksgerman@gmail.com>
2023-08-21 04:55:16 -0700

    Fix _printf.c

--- _printf.c
+++ _printf.c
@@ -11,46 +11,46 @@ void print_buffer(char output_buffer[], int *index);
 
 int _printf(const char *format, ...)
 {
-        int i, printed = 0, printed_chars = 0;
-        int format_flags, min_width, precision, size_spec, index = 0;
-        va_list list;
-        char output_buffer[BUFF_SIZE];
+	int i, printed = 0, printed_chars = 0;
+	int format_flags, min_width, precision, size_spec, index = 0;
+	va_list list;
+	char output_buffer[BUFF_SIZE];
 
-        if (format == NULL)
-                return (-1);
+	if (format == NULL)
+		return (-1);
 
-        va_start(list, format);
+	va_start(list, format);
 
-        for (i = 0; format && format[i] != '\0'; i++)
-        {
-                if (format[i] != '%')
-                {
-                        output_buffer[index++] = format[i];
-                        if (index == BUFF_SIZE)
-                                print_buffer(output_buffer, &index);
-                        printed_chars++;
-                }
-                else
-                {
-                        print_buffer(output_buffer, &index);
-                        format_flags = get_format_flags(format, &i);
-                        min_width = get_print_width(format, &i, list);
-                        precision = get_print_precision(format, &i, list);
-                        size_spec = get_size_specifier(format, &i);
-                        ++i;
-                        printed = handle_print(format, &i, list, output_buffer,
-                                                                   format_flags, min_width, precision, size_spec);
-                        if (printed == -1)
-                                return (-1);
-                        printed_chars += printed;
-                }
-        }
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			output_buffer[index++] = format[i];
+			if (index == BUFF_SIZE)
+				print_buffer(output_buffer, &index);
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(output_buffer, &index);
+			format_flags = get_format_flags(format, &i);
+			min_width = get_print_width(format, &i, list);
+			precision = get_print_precision(format, &i, list);
+			size_spec = get_size_specifier(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, output_buffer,
+								   format_flags, min_width, precision, size_spec);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
 
-        print_buffer(output_buffer, &index);
+	print_buffer(output_buffer, &index);
 
-        va_end(list);
+	va_end(list);
 
-        return (printed_chars);
+	return (printed_chars);
 }
 
 /**
@@ -61,9 +61,9 @@ int _printf(const char *format, ...)
 
 void print_buffer(char output_buffer[], int *index)
 {
-        if (*index > 0)
-                write(1, &output_buffer[0], *index);
+	if (*index > 0)
+		write(1, &output_buffer[0], *index);
 
-        *index = 0;
+	*index = 0;
 }
 
